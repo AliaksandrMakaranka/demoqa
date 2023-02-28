0f14eb737819563cba4e1c261b3600ac1430c080
Aliaksandr Makaranka <aliaksandrmakaranka@gmail.com>
2023-02-28 23:55:17 +0100

    add showSmallModal test with for

--- src/test/java/com/demoqa/pages/alertframewindows/ModalDialogsTest.java
+++ src/test/java/com/demoqa/pages/alertframewindows/ModalDialogsTest.java
@@ -2,11 +2,18 @@ package com.demoqa.pages.alertframewindows;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.Duration;
+import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 class ModalDialogsTest {
   WebDriver driver;
@@ -20,12 +27,25 @@ class ModalDialogsTest {
   }
 
   @Test
-  void setShowSmallModal() {
+  void showSmallModalTest() {
+    md.setShowSmallModal();
+    String text = driver.switchTo().activeElement().getText();
+    System.out.println(text);
 
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    List<WebElement> elements = wait.until(ExpectedConditions
+        .presenceOfAllElementsLocatedBy(By.cssSelector(".close")));
+
+    for (WebElement element : elements) {
+      if (element.isDisplayed()) {
+        element.click();
+      }
+    }
   }
 
   @Test
-  void setShowLargeModal() {
+  void showLargeModalTest() {
+    md.setShowLargeModal();
   }
 
   @AfterEach
